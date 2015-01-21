eb55bb4a247848d77657208ab1e0a8746205ff79
Jackson McGlasson <originalqwerty45@gmail.com>
2015-01-21 08:50:12 -0800

    added functions to hide and show images on index page

--- public_html/scripts.js
+++ public_html/scripts.js
@@ -1,3 +1,23 @@
 $("document").ready(function(){
-   $("#numbers").accordion({header: "h4"});
+    //binds picture to click and hide image function
+    $("#hideImage").bind("click", hideImage);
+    //binds picture to click and show image function
+    $("#showImage").bind("click", showImage);
 });
+// hides image
+function hideImage() {
+    $("#halo").animate({opacity: 0, height: "50px"}, 2000, "linear");
+    $("#slides").animate({opacity: 0, height: "50px"}, 2000, "linear");
+    $("#left").animate({opacity: 0, height: "50px"}, 2000, "linear");
+    $("#right").animate({opacity: 0, height: "50px"}, 2000, "linear");
+    $("#indicator").animate({opacity: 0, height: "50px"}, 2000, "linear");
+    
+}
+// shows image
+function showImage() {
+    $("#halo").animate({opacity: 100, height: "450px"}, 2000, "linear");
+    $("#slides").animate({opacity: 100, height: "500px"}, 2000, "linear");
+    $("#left").animate({opacity: 100, height: "500px"}, 2000, "linear");
+    $("#right").animate({opacity: 100, height: "500px"}, 2000, "linear");
+    $("#indicator").animate({opacity: 100, height: "500px"}, 2000, "linear");
+}
